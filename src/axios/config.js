3c99e7b7c1b455a7e259310ac2c8de3031865087
veshen <13120860906@163.com>
2019-06-30 00:10:17 +0800

    add axios

---
+++ src/axios/config.js
@@ -0,0 +1,15 @@
+//easy-mock模拟数据接口地址
+
+// PRODUCTION
+// const HOST                         = 'https://kk.sa-green.cn/';
+
+
+// test
+const HOST                      = 'http://yapi.demo.qunar.com/mock/73673/';
+
+/*
+    common
+*/
+
+//获取首页列表
+export const INDEX_LIST = HOST + 'api/getUserInfo';
---
+++ src/axios/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { get, post, postExport } from './tools';
+import * as config from './config';
+
+axios.defaults.withCredentials = true;
+/*
+    common
+*/
+
+export const getIndexList = () => get({url:config.INDEX_LIST});   //首页大盘 豆腐块
+
+// export const deldteEventStatus = (data) => post({url: config.DELDTE_EVENT_STATUS,data});//删除反馈列表
+
+// export const exportOrderQueryList = (data) => postExport({
+//     url: config.ORDER_LIST_EXPORT,
+//     data
+// }); //订单查询查询导出
+//
+// export const exportOrderQueryInterval = (data) => postExport({
+//     url: config.ORDER_INTERVAL_EXPORT,
+//     data
+// }); //订单查询查询导出 添加注释 进行插件测试
